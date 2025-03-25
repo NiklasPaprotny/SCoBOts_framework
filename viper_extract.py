d1cb28824aca0557b6f0812c250a7847d18d0ac3
anhth <anhthi.luu16@gmail.com>
2025-03-25 16:00:34 +0100

    working scobots rendering

--- viper_extract.py
+++ viper_extract.py
@@ -9,7 +9,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
 
 from scobi import Environment
-from ns_policies.SCoBOts_framework.utils.viper import VIPER
+from nsrl.policies.scobots.utils.viper import VIPER
 
 EVAL_ENV_SEED = 84
 
